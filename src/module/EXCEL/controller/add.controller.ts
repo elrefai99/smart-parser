3c7f44cdf19d3fcf877b63585f83184adc2b9af3
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-05-29 13:01:35 +0300

    fix: Correct function import for Excel file parsing in add controller

--- src/module/EXCEL/controller/add.controller.ts
+++ src/module/EXCEL/controller/add.controller.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../../../utils/asyncHandler.utils";
-import { parsePDF_Function } from "../../../script/parse/PDF/parse";
+import { parseExcel_Function } from "../../../script/parse/excel/parse";
 
 export const addConteroller = asyncHandler(
      async (req: Request, res: Response, _next: NextFunction) => {
           const file = req.file
-          const data = await parsePDF_Function(file?.path)
+          const data = await parseExcel_Function(file?.path)
 
           res.status(200).json({ code: 200, status: "OK", data })
      }
